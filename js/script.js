5579f8297b7a24bb18cb50a9dcef718c576733a0
Moustafa_Sherif <moustafasherif256@gmail.com>
2024-06-05 19:57:20 +0400

    About Us Animation

--- js/script.js
+++ js/script.js
@@ -24,13 +24,26 @@ window.onscroll = () => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
                         });
+        
+            // active sections for animation on scroll
+            sec.classList.add('show-animate');
         }
+
+        // if I want to use animation that repeats on scroll I should use this
+        else{
+            sec.classList.remove('show-animate')
+        }
+
     });
     //stichy header
     let header = document.querySelector('header');
 
     header.classList.toggle('sticky', window.scrollY > 100);
 
+
+    // remove toggle icon and navbar when click navbar links(scroll)
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 }
 
 
