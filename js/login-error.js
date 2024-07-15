96d914ae20b49645797a69ea313dd0ae9c13a8de
Moustafa_Sherif <moustafasherif256@gmail.com>
2024-07-15 18:56:16 +0400

    Adding SweetAlert

---
+++ js/login-error.js
@@ -0,0 +1,5 @@
+Swal.fire({
+    title: "Oops...",
+    text: "Sorry this function is not available at the moment",
+    icon: "error"
+  });
---
+++ js/sweetalert.js
@@ -0,0 +1,5 @@
+Swal.fire({
+    title: "Good News!",
+    text: "We have Successfully lanuched our new Portal ",
+    icon: "success"
+  });
