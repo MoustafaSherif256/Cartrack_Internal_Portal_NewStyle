98397b46f9a4511de2d77b2875c612c735a55d2a
Moustafa_Sherif <moustafasherif256@gmail.com>
2024-07-26 12:35:13 +0400

    Editing the Alert

--- js/alert.js
+++ js/alert.js
@@ -1,5 +1,5 @@
 Swal.fire({
     title: "Good News!",
-    text: "We have Successfully lanuched our New Portal ",
+    text: "CARTRACK is celebrating it's 10th anniversary today, so it's an ideal day to launch our Internal Portal.",
     icon: "success"
   });
